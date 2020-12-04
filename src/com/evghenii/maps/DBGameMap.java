2af064a9c1826b3ec89f971803663466268a85ba
Evghenii Costin <evghenii.costin@uni-oldenburg.de>
2020-12-04 15:18:41 +0100

    Maps for create from Textfile was created

---
+++ src/com/evghenii/maps/DBGameMap.java
@@ -0,0 +1,21 @@
+package com.evghenii.maps;
+
+import com.evghenii.abstractsGame.AbstractMapObject;
+
+public class DBGameMap extends AbstractMapObject {
+
+    @Override
+    public boolean drawMap() {
+        throw  new UnsupportedOperationException("No Support");
+    }
+
+    @Override
+    public boolean loadMap(Object o) {
+        throw  new UnsupportedOperationException("No Support");
+    }
+
+    @Override
+    public boolean saveMap(Object o) {
+        throw  new UnsupportedOperationException("No Support");
+    }
+}
