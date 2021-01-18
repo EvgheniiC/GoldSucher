91acea3cf146f6cace24b884f650d7cfcf772992
Evghenii Costin <evghenii.costin@uni-oldenburg.de>
2021-01-18 17:33:25 +0100

    FSScoreSave was added

---
+++ src/com/evghenii/score/impl/FSScoreSaver.java
@@ -0,0 +1,23 @@
+import com.evghenii.objects.UserScore;
+import com.evghenii.score.interfaces.ScoreSaver;
+
+import java.util.ArrayList;
+
+public class FSScoreSaver implements ScoreSaver {
+
+    @Override
+    public boolean saveScore(UserScore userScore) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public ArrayList<UserScore> getScoreList() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+  
+ 
+
+  
+
+}
